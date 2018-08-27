10ce721b8ccbe1b574788d95c26a3caddae401ab
m3mphiz <42731703+m3mphiz@users.noreply.github.com>
2018-08-27 10:08:07 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+//Смена изображения по клику
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/1.jpg') {
+      myImage.setAttribute ('src','images/2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/1.jpg');
+    }
+}
+
+//Ввод имени
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Введите имя:');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
